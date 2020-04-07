da7104354ec1d8de3cc2bbacd8d04bdc1f07b4d0
robin lei <13661603945@163.com>
2020-04-07 22:48:29 +0800

    添加client客户端

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'vant'
+export default {
+  components: {
+    [Button.name]: Button
+  }
+}
+</script>
