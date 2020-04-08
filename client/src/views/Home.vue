57fd171b04d881c6323aaf2c32f587156e5ceb27
robin lei <robin.lei@edenred.com>
2020-04-08 11:43:36 +0800

    首页Home完成

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,72 @@
 <template>
-  <div class="home">
-    <h1>home</h1>
+  <div class="homePage">
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+      >
+      <van-card
+        v-for="item in list"
+        :key="item.id"
+        :desc="item.summary"
+        :title="item.title"
+        :thumb="item.img"
+      >
+        <template #tags>
+          <p>{{item.content}}</p>
+        </template>
+      </van-card>
+    </van-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import {
+  List,
+  Image,
+  Card
+} from 'vant'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    [List.name]: List,
+    [Image.name]: Image,
+    [Card.name]: Card
+  },
+  data () {
+    return {
+      loading: false,
+      finished: false,
+      list: [
+        {
+          id: Math.floor(Math.random() * Math.floor(100000000)),
+          title: '思维风暴',
+          summary: '超级思维能力提升宝典',
+          content: '让思维更敏锐，让大脑更聪明，让人生更成功',
+          img: 'https://img.yzcdn.cn/vant/cat.jpeg'
+        },
+        {
+          id: Math.floor(Math.random() * Math.floor(100000000)),
+          title: '思维风暴',
+          summary: '超级思维能力提升宝典',
+          content: '让思维更敏锐，让大脑更聪明，让人生更成功',
+          img: 'https://img.yzcdn.cn/vant/cat.jpeg'
+        }
+      ]
+    }
+  },
+  methods: {
+    onLoad () {
+      this.loading = false
+    }
+  }
 }
 </script>
+<style lang="scss" scoped>
+.homePage {
+  .wrapper {
+    display: flex;
+  }
+}
+</style>
